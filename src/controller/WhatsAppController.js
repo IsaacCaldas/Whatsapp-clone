343ab9bb8df36181129d9d61a2732b34b2a48c0f
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2021-12-01 16:34:25 -0300

    Format id to camelCase

---
+++ src/controller/WhatsAppController.js
@@ -0,0 +1,16 @@
+class WhatsAppController {
+
+  constructor(){
+
+    this.loadElements();
+  }
+
+  loadElements(){
+
+    this.el = {};
+
+    document.querySelectorAll('[id]').forEach(element => {
+      this.el[Format.getCamelCase(element.id)] = element;
+    });
+  }
+}
