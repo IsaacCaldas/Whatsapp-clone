09631e2217ef3ee215b353ce4de8496fe13ca5ed
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2021-12-02 14:44:52 -0300

    Programado eventos dos botões do menu anexar

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -132,20 +132,59 @@ class WhatsAppController {
       this.el.menuAttach.addClass('open');
       document.addEventListener('click', this.closeMenuAttach.bind(this));
     });
+
     this.el.btnAttachPhoto.on('click', e =>{
-      console.log('photo');
+      this.el.inputPhoto.click();
     });
+    this.el.inputPhoto.on('change', e =>{
+      [...this.el.inputPhoto.files].forEach(file =>{
+
+      });
+    });
+
     this.el.btnAttachCamera.on('click', e =>{
-      console.log('Camera');
+      this.closeAllMainPanel();
+      this.el.panelCamera.addClass('open');
+      this.el.panelCamera.css({
+        'height':'105%'
+      })
     });
+    this.el.btnClosePanelCamera.on('click', e =>{
+      this.closeAllMainPanel();
+      this.el.panelMessagesContainer.show();
+    });
+    this.el.btnTakePicture.on('click', e =>{
+      console.log('tirou foto');
+    }); 
+
     this.el.btnAttachDocument.on('click', e =>{
-      console.log('Document');
+      this.closeAllMainPanel();
+      this.el.panelDocumentPreview.addClass('open');
+      this.el.panelDocumentPreview.css({
+        'height':'105%'
+      })
     });
+    this.el.btnClosePanelDocumentPreview.on('click', e =>{
+     this.closeAllMainPanel();
+      this.el.panelMessagesContainer.show();
+    });
+    this.el.btnSendDocument.on('click', e =>{
+      console.log('enviou arquivo');
+    });
+
     this.el.btnAttachContact.on('click', e =>{
-      console.log('Contact');
+      this.el.modalContacts.show();
+    });
+    this.el.btnCloseModalContacts.on('click', e =>{
+      this.el.modalContacts.hide();
     });
   }
 
+  closeAllMainPanel(){
+    this.el.panelMessagesContainer.hide();
+    this.el.panelDocumentPreview.removeClass('open'); 
+    this.el.panelCamera.removeClass('open');
+  }
   closeMenuAttach(e){
     document.removeEventListener('click', this.closeMenuAttach);
     this.el.menuAttach.removeClass('open');
