bc3cc1543eb7ff81ece164e2e17dce527728bc95
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2021-12-03 14:06:49 -0300

    Foto sendo tirada e mostrada com canvas

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -157,10 +157,31 @@ export class WhatsAppController {
     this.el.btnClosePanelCamera.on('click', e =>{
       this.closeAllMainPanel();
       this.el.panelMessagesContainer.show();
+      this._camera.stop();
     });
     this.el.btnTakePicture.on('click', e =>{
-      console.log('tirou foto');
+      let dataUrl = this._camera.takePicture();
+
+      this.el.pictureCamera.src = dataUrl;
+
+      this.el.pictureCamera.show();
+      this.el.videoCamera.hide();
+
+      this.el.btnReshootPanelCamera.show();
+      this.el.containerTakePicture.hide();
+      this.el.containerSendPicture.show();    
     }); 
+    this.el.btnReshootPanelCamera.on('click', e =>{
+      this.el.pictureCamera.hide();
+      this.el.videoCamera.show();
+      
+      this.el.btnReshootPanelCamera.hide();
+      this.el.containerTakePicture.show();
+      this.el.containerSendPicture.hide();    
+    });
+    this.el.btnSendPicture.on('click', e =>{
+      console.log(this.el.pictureCamera.src);
+    });
 
     this.el.btnAttachDocument.on('click', e =>{
       this.closeAllMainPanel();
