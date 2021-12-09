b59575f5d73578110f24285b4602b0a432e732e8
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2021-12-09 13:29:15 -0300

    Send photo as attachment

--- src/model/Message.js
+++ src/model/Message.js
@@ -54,15 +54,22 @@ export class Message extends Model {
     return new Promise((s, f)=> {
       let uploadTask = Firebase.hd().ref(from).child(Date.now()+ '_' + file.name).put(file);
 
-      uploadTask.on('state_change', e=>{
-        console.info('upload ', e);
+      uploadTask.on('state_changed', e=>{
+        //console.info('upload ', e);
       }, err =>{
         console.error(err);
       }, ()=>{
-        Message.send(this._contactActive.chatId, this._user.email, 'image', uploadTask.snapshot.dowloadURL).then(()=>{
-          s();
+        uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+          Message.send(
+              chatId, 
+              from, 
+              'image', 
+              downloadURL                    
+          ).then(() => {
+              s();
+          });
         });
-      }); 
+      });
     });
   }
 
@@ -157,11 +164,6 @@ export class Message extends Model {
                         <img src="${this.content}" class="_1JVSX message-photo" style="width: 100%; display:none">
                         <div class="_1i3Za"></div>
                     </div>
-                    <div class="message-container-legend">
-                        <div class="_3zb-j ZhF0n">
-                            <span dir="ltr" class="selectable-text invisible-space copyable-text message-text">Texto da foto</span>
-                        </div>
-                    </div>
                     <div class="_2TvOE">
                         <div class="_1DZAH text-white" role="button">
                             <span class="message-time">${Format.timeStampToTime(this.timeStamp)}</span>
@@ -179,9 +181,13 @@ export class Message extends Model {
             </div>
         </div>
         `;
-
+      
       div.querySelector('.message-photo').on('load', e =>{
-        console.log('load ok');
+        div.querySelector('.message-photo').show();
+        div.querySelector('._34Olu').hide();
+        div.querySelector('._3v3PK').css({
+          height:'auto'
+        });
       });
 
         break;
